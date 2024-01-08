f67e080f0d3979ffedb907a9e9d5d30117449804
code-hyun <122762287+code-hyun@users.noreply.github.com>
2024-01-08 22:47:18 +0900

    Flux & Mono

--- Rsocket_study/Client/src/main/java/com/rsocket/client/ClientRun.java
+++ Rsocket_study/Client/src/main/java/com/rsocket/client/ClientRun.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.Random;
 
 @Component
 public class ClientRun {
+    // RSocketRequester : Rsocket 연결을 추상화 하고 Rsocket 통신을 쉽게 수행 할 수 있도록 도와 주는 클래스
+    // 주로 서버와 클라이언트 간 메시지를 주고 받을 때 사용한다.
     private final RSocketRequester rSocketRequester;
 
     @Autowired
@@ -20,15 +23,16 @@ public class ClientRun {
         try {
             // 예제: request-response 메소드 호출
             requestResponse();
+            monoReqRes();
 
             // 예제: fire-and-forget 메소드 호출
-            fireAndForget();
+//            fireAndForget();
 
             // 예제: stream 메소드 호출
-            stream();
+//            stream();
 
             // 예제: channel 메소드 호출
-            channel();
+//            channel();
 
             // 프로그램 종료 전에 잠시 대기
             Thread.sleep(5000);
@@ -40,11 +44,29 @@ public class ClientRun {
     // request-response 메소드
     private void requestResponse() {
         // 서버로의 request-response 요청
+                // rSocketRequest.route - 경로 지정
         String response = rSocketRequester.route("req-res")
+                // .data() 보낼 메시지의 데이터
                 .data(new Message("superpil", "client"))
+                // Mono 사용하여 단일 응답(여기서는 String 형태로 응답 지정)
                 .retrieveMono(String.class)
+                // Mono 값에서 동기적으로 블록하고, 해당 값 반환
                 .block();
         System.out.println("Response from server: " + response);
+    }
+    private void monoReqRes() {
+        String str = "abcdefg!@#$%^";
+        Random r = new Random();
+        String id = "";
+        for (int i = 0; i<10; i++){
+            id += str.charAt(r.nextInt(12));
+        }
+        // 서버로의 Mono request-response 요청
+        String response = rSocketRequester.route("mono-req-res")
+                .data(id)
+                .retrieveMono(String.class)
+                .block();
+        System.out.println("req-res Mono test : " + response);
     }
 
     // fire-and-forget 메소드
