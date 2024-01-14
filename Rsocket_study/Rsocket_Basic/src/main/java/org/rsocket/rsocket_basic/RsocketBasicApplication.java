9c0427e422ed79b5da1f6939f97c1e0c789e3979
code-hyun <122762287+code-hyun@users.noreply.github.com>
2024-01-15 00:21:17 +0900

    commit

---
+++ Rsocket_study/Rsocket_Basic/src/main/java/org/rsocket/rsocket_basic/RsocketBasicApplication.java
@@ -0,0 +1,13 @@
+package org.rsocket.rsocket_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketBasicApplication.class, args);
+    }
+
+}
